7c0803e929a7f6e2bb00a7a409100acf8376d695
m.mehdi <m.mehdi@roomvu.co>
2022-07-24 22:38:39 +0430

    Typescript

--- cypress/e2e/progress/test.cy.js
+++ cypress/e2e/progress/test.cy.js
@@ -1,31 +1,31 @@
 
-import SignInPage from '../functions/signin.js';
+// import SignInPage from '../functions/signin.js';
 
-describe('Sign In', () => {
+// describe('Sign In', () => {
 
-  const signIn = new SignInPage();
+//   const signIn = new SignInPage();
 
-  beforeEach(() => {
-    signIn.loginLocal('mohammad.kh06@gmail.com','Dark@5680');
-    cy.visit("http://localhost:3000/agent-dashboard/leads-generation");
-    cy.get(
-      "[class^='news-list_newsList'] > :nth-child(1) > [class^='news-item_articleContainer']"
-    ).click();
+//   beforeEach(() => {
+//     signIn.loginLocal('mohammad.kh06@gmail.com','Dark@5680');
+//     cy.visit("http://localhost:3000/agent-dashboard/leads-generation");
+//     cy.get(
+//       "[class^='news-list_newsList'] > :nth-child(1) > [class^='news-item_articleContainer']"
+//     ).click();
 
-  })
+//   })
   
-  it('should sign in with correct credentials', () => {
+//   it('should sign in with correct credentials', () => {
  
-    const signIn = new SignInPage();
-    signIn.loginLocal();
+//     const signIn = new SignInPage();
+//     signIn.loginLocal();
 
-    // const signIn = home.goToSignIn();
+//     // const signIn = home.goToSignIn();
 
-    // signIn
-      // .fillEmail('john@doe.com')
-      // .fillPassword('123456')
-      // .submit();
+//     // signIn
+//       // .fillEmail('john@doe.com')
+//       // .fillPassword('123456')
+//       // .submit();
 
-    // home.getUserAvatar().should('exist');
-  });
-});
+//     // home.getUserAvatar().should('exist');
+//   });
+// });
